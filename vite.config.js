4e0364fefb6f3ffe4f7dad1171f3fe217da1b28d
ImranAdit <imran@adit.com>
2026-03-27 22:42:30 +0530

    Update API proxy URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': 'https://statement-analyzer-1-0.onrender.com'
     }
-  },
-  build: {
-    outDir: 'dist'
   }
 })
